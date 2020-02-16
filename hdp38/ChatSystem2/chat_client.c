863bd3a9ad4d5e40f40661982d60c4b10b14a3b6
Hoang-P <hdp38@xunil-03.coe.drexel.edu>
2020-02-15 21:41:40 -0500

    PROGRESS

--- hdp38/ChatSystem2/chat_client.c
+++ hdp38/ChatSystem2/chat_client.c
@@ -12,7 +12,6 @@
 */
 
 #define _POSIX_C_SOURCE 2 // For getopt()
-#define TEXT_LEN 2048
 #define SERVER_NAME  "/hdp38_njs76_chat_server"
 
 #include <mqueue.h>
@@ -25,6 +24,7 @@
 #include <string.h>
 #include "msg_structure.h"
 #include <signal.h>
+int sigKill = 0;
 
 void 
 print_main_menu (void)
@@ -34,6 +34,86 @@ print_main_menu (void)
     printf ("'E'xit\n");
     return;
 }
+
+static void setup_notification (mqd_t *mqdp);
+static void custom_signal_handler (int signalNumber);
+
+static void thread_func (union sigval sv) {
+    ssize_t nr;
+    mqd_t *mqdp;
+    void* msg_buffer;
+    struct server_msg *buffer;
+    struct mq_attr attr;
+
+    mqdp = sv.sival_ptr;
+
+    if (mq_getattr (*mqdp, &attr) == -1) {
+        perror ("mq_getattr");
+        exit (EXIT_FAILURE);
+    }
+
+    msg_buffer = malloc (attr.mq_msgsize);
+    if (msg_buffer == NULL) {
+        perror ("malloc");
+        exit (EXIT_FAILURE);
+    }
+
+    /* Reenable notification */
+    setup_notification (mqdp);
+
+    /* Drain the queue empty */
+    while ((nr = mq_receive (*mqdp, msg_buffer, attr.mq_msgsize, NULL)) >= 0) {
+        buffer = (struct server_msg *) msg_buffer;
+        printf ("%s: %s\n", buffer-> sender_name, buffer->msg);
+    }
+    free(msg_buffer);
+        
+
+    return;
+}
+
+
+static void 
+setup_notification (mqd_t *mqdp)
+{
+    struct sigevent sev;
+
+    sev.sigev_notify = SIGEV_THREAD;            /* Notify via thread */
+    sev.sigev_notify_function = thread_func;
+    sev.sigev_notify_attributes = NULL;
+    sev.sigev_value.sival_ptr = mqdp;           /* Argument to thread_func() */
+
+    if (mq_notify (*mqdp, &sev) == -1) {
+        perror ("mq_notify");
+        exit (EXIT_FAILURE);
+    }
+
+    return;
+}
+
+static void 
+mq_terminate (mqd_t mqd, char* client_name, struct client_msg msg)
+{
+    printf ("Chat client exiting\n");
+    /* Client MQ cleanup */
+    if (mq_send (mqd, (char *) &msg, sizeof (msg), 0) == -1) {
+        perror ("mq_send");
+        exit (EXIT_FAILURE);
+    }
+
+    if (mq_close(mqd) == (mqd_t)-1) {
+        perror ("mq_close");
+        exit (EXIT_FAILURE);
+    }
+    
+    if (mq_unlink(client_name) == (mqd_t)-1) {
+        perror ("mq_unlink");
+        exit (EXIT_FAILURE);
+    }
+
+    exit (EXIT_SUCCESS);
+}
+
 
 int 
 main (int argc, char **argv)
@@ -48,12 +128,14 @@ main (int argc, char **argv)
     strcpy (user_name, argv[1]); /* Get the client user name */
 
     struct client_msg msg;
+    struct client_msg server_msg;
     msg.client_pid = getpid();
     strcpy (msg.user_name, user_name);
     char option, dummy;
-    char recipient[TEXT_LEN];
+    char recipient[USER_NAME_LEN];
     char message[MESSAGE_LEN];
     msg.control = 1;
+    msg.broadcast = 2;
 
     /* Client MQs */
     int flags;
@@ -61,29 +143,35 @@ main (int argc, char **argv)
     mqd_t mqd;
     struct mq_attr attr;
     static char client_name[MESSAGE_LEN];
-    int c;
 
     /* Set the default message queue attributes. */
     attr.mq_maxmsg = 10;    /* Maximum number of messages on queue */
-    attr.mq_msgsize = 2048; /* Maximum message size in bytes */
+    attr.mq_msgsize = sizeof(server_msg); /* Maximum message size in bytes */
     flags = O_RDWR;         /* Create or open the queue for reading and writing */
     flags |= O_CREAT;
     flags |= O_NONBLOCK;
     
     perms = S_IRUSR | S_IWUSR;  /* rw------- permissions on the queue */
-    snprintf (client_name, MESSAGE_LEN, "/hdp38_njs76_client_%s.%i", (char*) user_name, (int) msg.client_pid);
+    snprintf (client_name, MESSAGE_LEN, "/hdp38_njs76_client_%s", (char*) user_name);
     mqd = mq_open (client_name, flags, perms, &attr);
     if (mqd == (mqd_t)-1) {
         perror ("mq_open");
         exit (EXIT_FAILURE);
     }
 
-    /* FIXME: Connect to server */
+    /* Set up notification */
+    setup_notification (&mqd);
+
+    /* Handle signal */
+    signal (SIGINT, custom_signal_handler);
+    signal (SIGUSR1, custom_signal_handler);
+
     mqd_t mqd_server;
     printf ("User %s connecting to server\n", user_name);
     mqd_server = mq_open (SERVER_NAME, O_WRONLY);
     if (mqd_server == (mqd_t)-1) {
         perror ("Server does not exist");
+        mq_terminate(mqd, client_name, msg);
         exit (EXIT_FAILURE);
     }
     
@@ -92,13 +180,25 @@ main (int argc, char **argv)
         perror ("mq_send");
         exit (EXIT_FAILURE);
     }
+   
     msg.control = 2;
-
+    sleep(2); /* Wait for response from server if is kill */
+    
+    if (sigKill == 1) {
+        msg.control = 0;
+        msg.broadcast = 2;
+        if (mq_send (mqd_server, (char *) &msg, sizeof (msg), 0) == -1) {
+            perror ("mq_send");
+            exit (EXIT_FAILURE);
+        }
+        printf("Server is full.\n");
+        mq_terminate(mqd, client_name, msg);
+    }
     while (1) {
         print_main_menu ();
         option = getchar ();
-        /* discard all characters up to and including newline */
-        while ((c = getchar()) != '\n' && c != EOF); 
+        /* Read dummy character to consume the \n left behind in STDIN */
+        dummy = getchar ();
 
         switch (option) {
             case 'B':
@@ -111,13 +211,14 @@ main (int argc, char **argv)
                     printf("\nOperation cancelled\n");
                     break;
                 };
-                message[strcspn(message, "\n")] = 0;
+                //message[strcspn(message, "\n")] = 0;
                 snprintf(msg.msg, sizeof(msg.msg), "%s", message);
                 //strcpy (msg.msg, message);
                 if (mq_send (mqd_server, (const char *) &msg, sizeof (msg), 0) == -1) {
                     perror ("mq_send");
                     exit (EXIT_FAILURE);
                 }
+                msg.broadcast = 2;
                 break;
 
             case 'P':
@@ -125,108 +226,62 @@ main (int argc, char **argv)
                  * message to server to be sent to private user */
                 msg.broadcast = 0;
                 printf("Username of recipient (Ctrl-d to cancel operation): ");
-                if (fgets(recipient, TEXT_LEN, stdin) == NULL)
+                if (fgets(recipient, USER_NAME_LEN, stdin) == NULL)
                 {
                     printf("Invalid User\n");
                     break;
                 };
-                recipient[strcspn(recipient, "\n")] = 0;
+                //recipient[strcspn(recipient, "\n")] = 0;
 
                 printf("Message to user (Ctrl-d to cancel operation): ");
-                if (fgets(message, TEXT_LEN, stdin) == NULL)
+                if (fgets(message, MESSAGE_LEN, stdin) == NULL)
                 {
                     printf("Operation cancelled\n");
                     break;
                 };
-                message[strcspn(message, "\n")] = 0;
+                //message[strcspn(message, "\n")] = 0;
                 snprintf(msg.msg, sizeof(msg.msg), "%s", message);
                 snprintf(msg.priv_user_name, sizeof(msg.priv_user_name), "%s", recipient);
-                if (mq_send (mqd, (char *) &msg, sizeof (msg), 1) == -1) {
+                if (mq_send (mqd_server, (char *) &msg, sizeof (msg), 1) == -1) {
                     perror ("mq_send");
                     exit (EXIT_FAILURE);
                 }
+                msg.broadcast = 2;
                 break;
 
-            case 'E':
-                printf ("Chat client exiting\n");
-                /* FIXME: Send message to server that we are exiting */
+            case 'E':    
                 msg.control = 0;
-                if (mq_send (mqd, (char *) &msg, sizeof (msg), 1) == -1) {
+                msg.broadcast = 2;
+                if (mq_send (mqd_server, (char *) &msg, sizeof (msg), 0) == -1) {
                     perror ("mq_send");
                     exit (EXIT_FAILURE);
                 }
-                if (mq_close(mqd) == (mqd_t)-1) {
-                    perror ("mq_close");
-                    exit (EXIT_FAILURE);
-                }
-                if (mq_unlink(client_name) == (mqd_t)-1) {
-                    perror ("mq_unlink");
-                    exit (EXIT_FAILURE);
-                }
-                exit (EXIT_SUCCESS);
-
+                mq_terminate(mqd, client_name, msg);
             default:
                 printf ("Unknown option\n");
                 break;
-        /* Read dummy character to consume the \n left behind in STDIN */
-        //dummy = getchar ();
         }
     }
-         
     exit (EXIT_SUCCESS);
 }
 
 
 static void 
-custom_signal_handler (int signalNumber, int pid, char* client_name)
+custom_signal_handler (int signalNumber)
 {
-    unsigned int priority;
-    void *buffer;
-    struct mq_attr attr;
-    ssize_t nr;
-    mqd_t mqd;
-    mqd = mq_open (client_name, O_RDONLY);
-    if (mqd == (mqd_t)-1) {
-        perror ("mq_open");
-        exit (EXIT_FAILURE);
-    }
-
     switch (signalNumber){
-        case SIGINT:
+        /*case SIGINT:
             printf ("Caught the Control+C signal. \n");
-            exit (EXIT_SUCCESS);
-
-        case SIGQUIT: 
-            printf ("Caught the Quit signal. \n");
-            printf ("Going through an orderly shutdown process. \n");
-            exit (EXIT_SUCCESS);
+            exit (EXIT_SUCCESS);*/
 
         case SIGUSR1:
-            /* Get the attributes of the MQ */
-            if (mq_getattr (mqd, &attr) == -1) {
-                perror ("mq_getattr");
-                exit (EXIT_FAILURE);
-            }
-
-            /* Allocate local buffer to store the received message from the MQ */
-            buffer = malloc (sizeof(attr.mq_msgsize));
-            if (buffer == NULL) {
-                perror ("malloc");
-                exit (EXIT_FAILURE);
-            }
-
-            nr = mq_receive (mqd, buffer, attr.mq_msgsize, &priority);
-            if (nr == -1) {
-                perror ("mq_receive");
-                exit (EXIT_FAILURE);
-            }
-
-            printf ("Read %ld bytes; priority = %u \n", (long) nr, priority);
-            if (write (STDOUT_FILENO, buffer, nr) == -1) {
-                perror ("write");
-                exit (EXIT_FAILURE);
-            }
-
+            sigKill = 1;
+            //exit (EXIT_SUCCESS);
+            break;
+        case SIGINT:
+            sigKill = 1;
+            //exit (EXIT_SUCCESS);
+            break;
          default: 
              break;
 
--- hdp38/ChatSystem2/chat_server.c
+++ hdp38/ChatSystem2/chat_server.c
@@ -8,9 +8,16 @@
  * Student/team name: FIXME
  * Date created: FIXME  
  *
+ * 
+ * Server will only send a message FROM the server if the server is full 
+ * or if the client tries to connect with same name as server.
+ * Otherwise, message "sender" is the client who originated the message. 
  */
+#define _POSIX_C_SOURCE 1
 
 #define SERVER_NAME  "/hdp38_njs76_chat_server"
+#define MAX_CLIENTS 5
+#define CTRLC 10
 
 #include <mqueue.h>
 #include <sys/stat.h>
@@ -20,23 +27,39 @@
 #include <stdio.h>
 #include <errno.h>
 #include "msg_structure.h"
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <setjmp.h>
 
 /* FIXME: Establish signal handler to handle CTRL+C signal and 
  * shut down gracefully. 
  */
+static void custom_signal_handler (int signalNumber);
+static sigjmp_buf env;
 
 int 
 main (int argc, char **argv)
 {
     int flags;
     mode_t perms;
-    mqd_t mqd;
+    mqd_t mqd; /* Server MQ */
     struct mq_attr attr;
-    struct client_msg buffer;
+    struct client_msg msg_buffer;
+    char connected_clients[MAX_CLIENTS][USER_NAME_LEN];
+    memset(connected_clients, '\0', sizeof(connected_clients[0][0]) * MAX_CLIENTS * USER_NAME_LEN);
+
+    mqd_t priv_mq; /* mq for private user */
+    mqd_t broadcast_mq;
+    struct server_msg server_buffer;
+    /* Build the dest_user mq */
+    static char dest_user_mq[MESSAGE_LEN];
+    int num_clients = 0;
+
 
     /* Set the default message queue attributes. */
     attr.mq_maxmsg = 10;    /* Maximum number of messages on queue */
-    attr.mq_msgsize = sizeof(buffer); /* Maximum message size in bytes */
+    attr.mq_msgsize = sizeof(msg_buffer); /* Maximum message size in bytes */
     flags = O_RDWR;         /* Create or open the queue for reading and writing */
     flags |= O_CREAT;
     flags |= O_NONBLOCK;
@@ -48,11 +71,41 @@ main (int argc, char **argv)
         perror ("mq_open");
         exit (EXIT_FAILURE);
     }
+    /* Handle Ctrl-C signal */
+    signal (SIGINT, custom_signal_handler);
 
-    //mqd_t mqd;
     unsigned int priority;
     //struct mq_attr attr;
     ssize_t nr;
+
+    int ret;
+    ret = sigsetjmp (env, 1);
+
+    /* Switch statement for sigsetjmp */
+    switch (ret)
+    {
+        case 0:
+            break;
+
+        case CTRLC:
+            printf("\nServer gracefully shutting down.\n");
+            if (mq_close(mqd) == (mqd_t)-1) {
+                perror ("mq_close");
+                exit (EXIT_FAILURE);
+            }
+            
+            if (mq_unlink(SERVER_NAME) == (mqd_t)-1) {
+                perror ("mq_unlink");
+                exit (EXIT_FAILURE);
+            }
+            exit(EXIT_SUCCESS);
+        
+        default:
+            break;
+    }
+
+
+
     while (1) {
         /* FIXME: Server code here */
         /* Get the attributes of the MQ */
@@ -62,17 +115,136 @@ main (int argc, char **argv)
         }
 
         /* Allocate local buffer to store the received message from the MQ */
-        nr = mq_receive (mqd, (char *) &buffer, sizeof(buffer), &priority);
+        nr = mq_receive (mqd, (char *) &msg_buffer, sizeof(msg_buffer), &priority);
         if (nr == -1) {
             //perror ("mq_receive");
             continue;
         }
 
-        if (buffer.control != 1)
-        {
-            printf ("Read %ld bytes; priority = %u \n", (long) nr, priority);
-            printf ("%s \n", buffer.msg);
+
+        /* Add user to the connected clients array */
+        switch(msg_buffer.control) {
+            case 0: /* User leaves */
+                printf("DEBUG: User leaving\n");
+                for (int i = 0; i < MAX_CLIENTS; i++) {
+                    // printf("%s: %s\n", connected_clients[i], msg_buffer.user_name);
+                    if (strcmp(connected_clients[i], msg_buffer.user_name) == 0) {
+                        num_clients--;
+                        printf("NumClients : %i\n", num_clients);
+                        printf("DEBUG: The user that left was %s\n", msg_buffer.user_name);
+                        memset(&connected_clients[i], '\0', sizeof(connected_clients[i]));
+                        
+                        // for (int ii = 0; ii < MAX_CLIENTS; ii++) {
+                        //     printf("DEBUG: Client %i: %s\n", ii, connected_clients[ii]);
+                        // }
+                        break;
+                    }
+                }
+                break;
+            case 1: /* User joins */
+                // printf("DEBUG: User joins\n");             
+                /* Check to see if server is full */
+                num_clients++;
+                if (num_clients > MAX_CLIENTS) {
+                    printf("Server full! \nKilling client: %s\n", msg_buffer.user_name);
+                    num_clients--;
+                    kill(msg_buffer.client_pid, SIGUSR1);
+                    break;
+                }
+
+                for (int i = 0; i < MAX_CLIENTS; i++) {
+                    if (connected_clients[i][0] == 0) {
+                        snprintf(connected_clients[i], sizeof(connected_clients[i]), "%s", msg_buffer.user_name);
+                        printf("DEBUG: The user that joined was %s\n", connected_clients[i]);
+                        for (int ii = 0; ii < MAX_CLIENTS; ii++) {
+                            printf("DEBUG: Client %i: %s\n", ii, connected_clients[ii]);
+                        }
+                        printf("NumClients : %i\n", num_clients);
+                        break;
+                    }
+                }
+                break;
+            default:
+                printf("DEBUG: Unknown case for control: %d\n", msg_buffer.control);
+                break;
         }
+
+        switch (msg_buffer.broadcast) {
+            case 0: /* private message */
+                /* find the user to whisper */
+                printf("private message\n");
+                snprintf (dest_user_mq, MESSAGE_LEN, "/hdp38_njs76_client_%s", (char *) msg_buffer.priv_user_name);
+                dest_user_mq[strcspn(dest_user_mq, "\n")] = 0; // remove newline from client name
+                printf("Opened priv_mq of : %s\n", dest_user_mq);
+                priv_mq = mq_open (dest_user_mq, O_WRONLY);
+                
+                /* Open priv user mq and send */
+                if (priv_mq == (mqd_t)-1) {
+                    perror ("Could not find private message recipient");
+                    // exit (EXIT_FAILURE);
+                    break;
+                }
+
+                strcpy(server_buffer.sender_name, msg_buffer.user_name);
+                strcpy(server_buffer.msg, msg_buffer.msg);
+                if (mq_send (priv_mq, (char *) &server_buffer, sizeof (server_buffer), 0) == -1) {
+                    perror ("mq_send");
+                    exit (EXIT_FAILURE);
+                }
+                printf("Message from %s sent to other person %s.\nContents: %s\n", server_buffer.sender_name, \
+                                                              msg_buffer.priv_user_name, server_buffer.msg);
+
+                break;
+
+            case 1: /* broadcast message */
+                printf("Broadcast message from %s\n", msg_buffer.user_name);
+                /* loop over each client that currently exists. Also ignore the client that's sending the message. */
+                for (int i = 0; i < MAX_CLIENTS; i++) {
+                    if (connected_clients[i][0] != 0 && strcmp(connected_clients[i], msg_buffer.user_name) != 0) { 
+                        snprintf (dest_user_mq, MESSAGE_LEN, "/hdp38_njs76_client_%s", (char *) connected_clients[i]);
+                        dest_user_mq[strcspn(dest_user_mq, "\n")] = 0; // remove newline from client name
+                        printf("Sending to mq : %s\n", dest_user_mq);
+                        broadcast_mq = mq_open (dest_user_mq, O_WRONLY);
+
+                        /* Open priv user mq and send */
+                        if (broadcast_mq == (mqd_t)-1) {
+                            perror ("Could not find message recipient");
+                            continue;
+                        }
+
+                        strcpy(server_buffer.sender_name, msg_buffer.user_name);
+                        strcpy(server_buffer.msg, msg_buffer.msg);
+                        if (mq_send (broadcast_mq, (char *) &server_buffer, sizeof (server_buffer), 0) == -1) {
+                            perror ("mq_send");
+                            exit (EXIT_FAILURE);
+                        }
+                        printf("Message from %s sent to other person %s.\nContents: %s\n", server_buffer.sender_name, \
+                                                                    connected_clients[i], server_buffer.msg);
+
+                    }
+
+                }
+                break;
+            default:
+                // printf("DEBUG: Unknown case for broadcast: %d\n", msg_buffer.broadcast);
+                break;
+
+        }
+
+        
+
+        // if (msg_buffer.control == 2)
+        // {
+        //     printf ("Read %ld bytes; priority = %u \n", (long) nr, priority);
+        //     printf ("%s \n", msg_buffer.msg);
+        // }
     }
     exit (EXIT_SUCCESS);
+}
+
+
+static void 
+custom_signal_handler (int signalNumber)
+{
+    siglongjmp (env, CTRLC);
 }
